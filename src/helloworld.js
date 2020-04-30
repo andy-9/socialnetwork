1ea9dfcbb18317c540443455c3187c73c88b01fa
Andreas Hechler <hechler@jpberlin.de>
2020-04-30 12:05:21 +0200

    code from today

---
+++ src/helloworld.js
@@ -0,0 +1,58 @@
+// src/helloworld.js // could be named anything, but convention: name after what is inside
+import React from "react";
+import axios from "axios"; // needs to be imported, is not global
+import Child from "./child";
+
+export default class HelloWorld extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            first: "Andy",
+            last: "Hechler",
+        };
+    }
+
+    componentDidMount() {
+        // oftentimes in Vue we would do the following in mounted:
+        // axios.get("/images").then(function (resp) {
+        //     self.images = resp.data;
+        // });
+        axios.get("/some-route").then((resp) => {
+            // this is how to update state (always with setState):
+            this.setState({
+                // first: resp.data.first, // data is what we get from the server
+            });
+        });
+
+        setTimeout(() => {
+            this.setState({
+                first: "Andreas",
+            });
+        }, 3000);
+    }
+
+    // this becomes undefined when we create our own methods, we need to bind it
+    handleClick() {
+        console.log("handleClick is running!");
+        this.setState({
+            first: "animal crossing",
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                Hello, {this.state.first} {this.state.last}!
+                <Child last={this.state.last} />
+                {/* prop: "last", passed down to child
+                "last" on left side could be anything, is the name of the prop */}
+                <p onClick={() => this.handleClick()}>click me!</p>
+                {/* arrow-functions remember "this" --> we can use it in our event handlers (here: handleClick) */}
+            </div>
+        );
+    }
+}
+
+// export default function HelloWorld() {
+//     return <div>Hello, World!</div>;
+// }
