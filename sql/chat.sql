5c22d7ea9c50f74be12b83e93a48b8a8dfe51f94
Andreas Hechler <hechler@jpberlin.de>
2020-08-23 00:15:03 +0200

    deleted default values for chat-table

--- sql/chat.sql
+++ sql/chat.sql
@@ -7,5 +7,3 @@ CREATE TABLE chat (
     receiver_id INT REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
  );
-
-INSERT INTO chat (text, user_id, receiver_id) VALUES ('What a wonderful idea!', '200', '1'),('Not gonna get me!', '1', '2'),('I am tired, going to bed now', '2', '200'),('Hello there!', '2', '1'),('let me outta here', '200', '2'),('You look beautiful', '1', '200'),('Fixing to eat', '2', '1'),('Getting dressed for tonight', '3', '1')
